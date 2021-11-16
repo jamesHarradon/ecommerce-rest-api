b4f70d82a2ec48f0b6770781e9c5a1b64496553c
James Harradon <james.harradon@gmail.com>
2021-11-16 17:28:59 +0000

    add heroku url to swagger

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:3000"
+                url: "https://jims-ecommerce-rest-api.herokuapp.com/"
             }
         ],
     },
